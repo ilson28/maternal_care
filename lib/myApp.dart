fab9d46aaaa331a39c5de50b6ec5ef992c36ac29
david_morelo <elquesabe2003@gmail.com>
2025-08-20 20:51:06 +0000

    feat(welcome): add screeen Welcome.

---
+++ lib/myApp.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:maternalcare/screens/welcome.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Application name
+      title: 'Maternal care',
+      // Application theme data, you can set the colors for the application as
+      // you want
+      theme: ThemeData(
+        // useMaterial3: false,
+        primarySwatch: Colors.blue,
+      ),
+      // A widget which will be started on application startup
+      home: Scaffold(
+        body: Welcome(),
+      ),
+    );
+  }
+}
